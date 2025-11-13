bedd0455279dc15163aed2e5713dc16345c04039
Alanna Tempest <alannayt@gmail.com>
2025-11-13 18:22:00 -0500

    bpe: tests pass! next step scale it
    test command `uv run pytest tests/test_train_bpe.py`

--- cs336_basics/bpe.py
+++ cs336_basics/bpe.py
@@ -1,7 +1,51 @@
 import os
 from collections import defaultdict
 import regex as re
-from pretokenization_example import find_chunk_boundaries
+from typing import Generator, Any
+
+
+def pretokenize(chunk: str, special_tokens: list[str]) -> dict[tuple[bytes, ...], int]:
+    counts = defaultdict(int)
+    special_tokens_pattern = '|'.join(re.escape(token) for token in special_tokens)
+    docs = re.split(special_tokens_pattern, chunk)
+    PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
+    for doc in docs:
+        for match in re.finditer(PAT, doc):
+            counts[tuple(bytes([b]) for b in match.group().encode("utf-8"))] += 1
+    return counts
+
+
+def byte_pairs(data: tuple[bytes, ...]) -> Generator[tuple[bytes, bytes], Any, Any]:
+    """Yield consecutive pairs of bytes objects from a tuple of bytes objects."""
+    for i in range(len(data)-1):
+        yield (data[i], data[i+1])
+
+
+def tuple_contains(t: tuple, subt: tuple) -> int:
+    assert len(subt) == 2 # Code is written gneerically but only tested on len 2
+    for i in range(len(t)-len(subt)+1):
+        for subi in range(len(subt)):
+            if t[i + subi] != subt[subi]:
+                break
+            if subi == len(subt)-1:
+                return i
+    return -1
+
+
+def merge_bp(tokens: dict[tuple[bytes, ...], int], byte_pair: tuple[bytes, bytes]):
+    merged = False
+    merged_bp = (byte_pair[0] + byte_pair[1],)
+    for token in list(tokens.keys()):
+        found = tuple_contains(token, byte_pair)
+        while (found >= 0):
+            merged = True
+            new_token = token[0:found] + merged_bp + token[found+2:]
+            #print(f"replacing {token} with {new_token}")
+            tokens[new_token] = tokens[token]
+            tokens.pop(token)
+            token = new_token
+            found = tuple_contains(token, byte_pair)
+    return tokens, merged
 
 
 def run_train_bpe(
@@ -31,38 +75,58 @@ def run_train_bpe(
                 representing that <token1> was merged with <token2>.
                 Merges are ordered by order of creation.
     """
-    max_file_size = 5_000_000
+    #max_file_size = 5_000_000
     with open(input_path, "rb") as f:
-        if os.path.getsize(input_path) > max_file_size:
-            print(f"WARN: file was {os.path.getsize(input_path)}, only reading first 5MB")
-            chunk = f.read(max_file_size).decode("utf-8", errors="ignore")
-        else:
-            chunk = f.read().decode("utf-8", errors="ignore")
-        """
-        num_processes = 4
-        boundaries = find_chunk_boundaries(f, num_processes, b"<|endoftext|>")
+    #    if os.path.getsize(input_path) > max_file_size:
+    #        print(f"WARN: file was {os.path.getsize(input_path)}, only reading first 5MB")
+    #        chunk = f.read(max_file_size).decode("utf-8", errors="ignore")
+    #    else:
+        chunk = f.read().decode("utf-8", errors="ignore")
 
-        # The following is a serial implementation, but you can parallelize this
-        # by sending each start/end pair to a set of processes.
-        for start, end in zip(boundaries[:-1], boundaries[1:]):
-            f.seek(start)
-            chunk = f.read(end - start).decode("utf-8", errors="ignore")
-            # Run pre-tokenization on your chunk and store the counts for each pre-token
+    vocab = { i : bytes([i]) for i in range(256) }
+    next_vocab_ix = 256
+    for st in special_tokens:
+        vocab[next_vocab_ix] = st.encode("utf-8") # should this be encoded? does it matter?
+        next_vocab_ix += 1
+
+    pretokens = pretokenize(chunk, special_tokens)
+    #for k, v in list(pretokens.items())[:10]:
+    #    print(k, v)
+
+    merges = []
+    print_counter = 0
+    while len(vocab) < vocab_size:
+        if (print_counter % 100 == 0):
+            print(f"starting iter {print_counter}")
+        print_counter += 1
+
+        pair_counts: dict[tuple[bytes, bytes], int] = defaultdict(int)
+        # iterate through pretokens
+        for pt in pretokens:
+            for bp in byte_pairs(pt):
+                pair_counts[bp] += pretokens[pt]
+        # Find max pair
+        max_count = 0
+        max_pair = (b'\x00', b'\x00')
+        for pair, count in pair_counts.items():
+            if count > max_count:
+                max_count = count
+                max_pair = pair
+            if count == max_count and pair > max_pair:
+                max_pair = pair
+
+        pretokens, merged = merge_bp(pretokens, max_pair)
+        if not merged:
+            print(f"WARN: no merge for pair {max_pair}. logic error?")
             break
-        """
+        vocab[next_vocab_ix] = max_pair[0] + max_pair[1]
+        next_vocab_ix += 1
+        merges.append(max_pair)
 
-    counts = defaultdict(int)
-    special_tokens_pattern = '|'.join(re.escape(token) for token in special_tokens)
-    docs = re.split(special_tokens_pattern, chunk)
-    PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
-    for doc in docs:
-        for match in re.finditer(PAT, doc):
-            counts[tuple(bytes([b]) for b in match.group().encode("utf-8"))] += 1
+        #print(max_pair)
+        #print(pretokens)
 
-    for k, v in list(counts.items())[:10]:
-        print(k, v)
-
-    return {}, []
+    return vocab, merges
 
 
 def main():
