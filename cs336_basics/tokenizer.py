aad70bbb3f963ce4d3f47ca1db6ff945bcc38304
Alanna Tempest <alannayt@gmail.com>
2025-11-18 21:56:55 -0500

    tok: 11/25 tests pass
    todo: special tokens handling
    command: uv run pytest tests/test_tokenizer.py

--- cs336_basics/tokenizer.py
+++ cs336_basics/tokenizer.py
@@ -10,6 +10,7 @@ class Tokenizer:
         special_tokens: list[str] | None = None
     ):
         self.vocab = vocab
+        self.rvocab = { v : k for k, v in vocab.items() }
         self.merges = merges
         self.special_tokens = special_tokens
 
@@ -35,6 +36,29 @@ class Tokenizer:
 
         return cls(vocab, merges, special_tokens)
 
+    def _tuple_contains(self, t: tuple, subt: tuple) -> int:
+        # assert len(subt) == 2
+        for i in range(len(t)-1):
+            for subi in range(2):
+                if t[i + subi] != subt[subi]:
+                    break
+                if subi == len(subt)-1:
+                    return i
+        return -1
+
+    def _merge_bp(self, tokens, pair):
+        merged_bp = (pair[0] + pair[1],)
+        new_tokens = []
+        for token in tokens:
+            # mutate a copy - does this matter?
+            new_token = token
+            found = self._tuple_contains(new_token, pair)
+            while (found >= 0):
+                new_token = new_token[0:found] + merged_bp + new_token[found+2:]
+                found = self._tuple_contains(new_token, pair)
+            new_tokens.append(new_token)
+        return tokens
+
     def encode(self, text: str) -> list[int]:
         # pretokenize
         pretokens = []
@@ -43,8 +67,15 @@ class Tokenizer:
         for match in re.finditer(PAT, text):
             pretokens.append(tuple(bytes([b]) for b in match.group().encode("utf-8")))
         # merge
-        # TODO
-        return []
+        tokens = pretokens
+        for pair in self.merges:
+            tokens = self._merge_bp(tokens, pair)
+        # get ints
+        encoded = []
+        for token in tokens:
+            for bytegroup in token:
+                encoded.append(self.rvocab[bytegroup])
+        return encoded
 
     def encode_iterable(self, iterable: Iterable[str]) -> Iterator[int]:
         for s in iterable:
